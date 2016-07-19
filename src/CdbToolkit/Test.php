2f1a527e7d626984c651e7d7df6c3e29f56004ff
Renato Souza <brt.renatos@gmail.com>
2016-07-19 17:14:13 +0000

    test

---
+++ src/CdbToolkit/Test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: renatosouza
+ * Date: 7/19/16
+ * Time: 2:13 PM
+ */
+
+class Test
+{
+    public function ble()
+    {
+        return 'oi';
+    }
+
+}
